1308a40973d4fbbb2f0bf6eda5219607d15e4418
Aayush Ghimire <aausgh@gmail.com>
2024-06-23 21:50:46 +0545

    setup swagger api

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mesh_UI Components API') // Set the title of the API
+    .setDescription('Mesh_UI Components API description') // Set the description of the API
+    .setVersion('0.1') // Set the version of the API
+    .build(); // Build the document
+
+  // Create a Swagger document using the application instance and the document configuration
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Setup Swagger module with the application instance and the Swagger document
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
